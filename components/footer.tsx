10d504eb04510f5db7e2fa66c18ae9ef41e6f9da
Muzammil <memon.muzammil24@gmail.com>
2025-01-30 21:05:59 +0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,11 +6,6 @@ export default function Footer() {
       <small className="mb-2 block text-xs">
         &copy; 2025. All rights reserved.
       </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js , TypeScript, Tailwind CSS, Framer Motion, React Email &
-        Resend, Vercel hosting.
-      </p>
     </footer>
   );
 }
