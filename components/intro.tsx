008367df54d9b5cbf3fa41fa511d9a4f24d2563e
Muzammil <memon.muzammil24@gmail.com>
2025-03-25 10:39:04 +0500

    bug fixes

--- components/intro.tsx
+++ components/intro.tsx
@@ -66,7 +66,7 @@ export default function Intro() {
         <span className="font-bold">Hello, I&apos;m Muzammil.</span> I&apos;m an{" "}
         <span className="font-bold">AI and Machine Learning enthusiast</span>{" "}
         with <span className="font-bold">2 years</span> of experience, driven by
-        a deep passion for innovation. I’m also a{" "}
+        a deep passion for innovation. I'm also a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">1 year</span> of experience, creating{" "}
         <span className="font-bold">dynamic websites</span>. My expertise lies
@@ -84,7 +84,7 @@ export default function Intro() {
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-105 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
@@ -95,7 +95,7 @@ export default function Intro() {
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-105 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/CV.pdf"
           download
         >
@@ -111,13 +111,13 @@ export default function Intro() {
           <BsLinkedin />
         </a>
 
-        {/* <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/muzammilexe"
           target="_blank"
         >
           <FaGithubSquare />
-        </a> */}
+        </a>
       </motion.div>
     </section>
   );
